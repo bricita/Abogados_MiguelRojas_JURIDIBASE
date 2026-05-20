f8da12acc33a98a95bfeeb1abc30af896fc12600
Rebazzza <victorreba16@gmail.com>
2026-05-19 21:33:22 -0500

    Modelo Pago, ServicioLegal y Usuario, Creacion DbSet en DbContext

--- Abogados_MiguelRojas_JURIDIBASE/Models/Pago.cs
+++ Abogados_MiguelRojas_JURIDIBASE/Models/Pago.cs
@@ -1,6 +1,18 @@
-﻿namespace Abogados_MiguelRojas_JURIDIBASE.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Abogados_MiguelRojas_JURIDIBASE.Models
 {
     public class Pago
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idPago { get; set; }
+        [Required, StringLength(50)]
+        public string metodoPago {  get; set; }
+        public float monto { get; set; }
+        public DateOnly fechaPago { get; set; }
+        public Caso caso {  get; set; }
+
+        
     }
 }
--- Abogados_MiguelRojas_JURIDIBASE/Models/ServicioLegal.cs
+++ Abogados_MiguelRojas_JURIDIBASE/Models/ServicioLegal.cs
@@ -1,6 +1,18 @@
-﻿namespace Abogados_MiguelRojas_JURIDIBASE.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Abogados_MiguelRojas_JURIDIBASE.Models
 {
     public class ServicioLegal
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idServicio {  get; set; }
+        [Required, StringLength(100)]
+        public string nombre { get; set; }
+        [Required, StringLength(200)]
+        public string descripcion {  get; set; }
+        [Required, StringLength(100)]
+        public string estado { get; set; }  
+        public float costoBase { get; set; }
     }
 }
